4ea5c4d9b7433322711c164a2105b6ff09b033b8
ArchitKC <archithrd@gmail.com>
2020-01-23 07:01:47 -0500

    Initial commit

---
+++ CandidateProject/ViewModels/CartonViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CandidateProject.ViewModels
+{
+    public class CartonViewModel
+    {
+        public int Id { get; set; }
+        public string CartonNumber { get; set; }
+
+        public int count { get; set; }
+    }
+}
